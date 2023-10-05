162a26bef27a319d00e312cfd73c44b9db22acba
VNKaiser <tandatvok16@gmail.com>
2023-10-05 07:53:29 +0700

    init

---
+++ HamKT.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace UnitTestBDCL1
+{
+    class HamKT
+    {
+        public String Triangle(int a, int b, int c)
+        {
+            int match = 0;
+            if (a == b)
+                match = match + 1;
+            if (a == c)
+                match = match + 2;
+            if (b == c)
+                match = match + 3;
+            if (match == 0)
+                if ((a + b) <= c)
+                    return ("");
+                else if ((b + c) <= a)
+                    return ("");
+                else if ((a + c) <= b)
+                    return ("");
+                else return ("Scalene");
+            else if (match == 1)
+                if ((a + c) <= b)
+                    return ("");
+                else return ("Isosceles");
+            else if (match == 2)
+                if ((a + c) <= b)
+                    return ("");
+                else return ("Isosceles");
+            else if (match == 3)
+                if ((b + c) <= a)
+                    return ("");
+                else return ("Isosceles");
+            else return ("Equilateral");
+
+        }
+
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestBDCL1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestBDCL1")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f9f29556-ed92-49d1-9a06-62faa7a9a924")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

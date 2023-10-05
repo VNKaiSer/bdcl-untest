9e9c928b7cef1487b148d97c9b2d6f141ac4e368
VNKaiser <tandatvok16@gmail.com>
2023-10-05 08:47:13 +0700

    finnish checkLeapYear

--- TestCheckLeapYear.cs
+++ TestCheckLeapYear.cs
@@ -7,8 +7,56 @@ namespace UnitTestBDCL1
     public class UnitTest3
     {
         [TestMethod]
-        public void TestMethod1()
+        public void VP1()
         {
+            UnitTestBDCL1.CheckLeapYear clsLeapYear = new UnitTestBDCL1.CheckLeapYear();
+            bool act_Triangle = clsLeapYear.IsLeapYear(1720);//Kết quả thực a,b,c=3
+           // bool exp_Triangle = true; // kết quả mong đợi
+            Assert.IsTrue(act_Triangle, "Sai roi ne"); // hàm so sánh
         }
+
+        [TestMethod]
+        public void VP2()
+        {
+            UnitTestBDCL1.CheckLeapYear clsLeapYear = new UnitTestBDCL1.CheckLeapYear();
+            bool act_Triangle = clsLeapYear.IsLeapYear(1750);//Kết quả thực a,b,c=3
+                                                             // bool exp_Triangle = true; // kết quả mong đợi
+            Assert.IsFalse(act_Triangle, "Sai roi ne"); // hàm so sánh
+        }
+
+        [TestMethod]
+        public void IVP1()
+        {
+            UnitTestBDCL1.CheckLeapYear clsLeapYear = new UnitTestBDCL1.CheckLeapYear();
+            bool act_Triangle = clsLeapYear.IsLeapYear(1234);//Kết quả thực a,b,c=3
+                                                             // bool exp_Triangle = true; // kết quả mong đợi
+            Assert.IsFalse(act_Triangle, "Sai roi ne"); // hàm so sánh
+        }
+
+        [TestMethod]
+        public void VB1()
+        {
+            UnitTestBDCL1.CheckLeapYear clsLeapYear = new UnitTestBDCL1.CheckLeapYear();
+            bool act_Triangle = clsLeapYear.IsLeapYear(1582);//Kết quả thực a,b,c=3
+                                                             // bool exp_Triangle = true; // kết quả mong đợi
+            Assert.IsFalse(act_Triangle, "Sai roi ne"); // hàm so sánh
+        }
+        [TestMethod]
+        public void VB2()
+        {
+            UnitTestBDCL1.CheckLeapYear clsLeapYear = new UnitTestBDCL1.CheckLeapYear();
+            bool act_Triangle = clsLeapYear.IsLeapYear(1583);//Kết quả thực a,b,c=3
+                                                             // bool exp_Triangle = true; // kết quả mong đợi
+            Assert.IsFalse(act_Triangle, "Sai roi ne"); // hàm so sánh
+        }
+        [TestMethod]
+        public void IVB()
+        {
+            UnitTestBDCL1.CheckLeapYear clsLeapYear = new UnitTestBDCL1.CheckLeapYear();
+            bool act_Triangle = clsLeapYear.IsLeapYear(1281);//Kết quả thực a,b,c=3
+                                                         // bool exp_Triangle = true; // kết quả mong đợi
+            Assert.IsFalse(act_Triangle, "Sai roi ne"); // hàm so sánh
+        }
+
     }
 }

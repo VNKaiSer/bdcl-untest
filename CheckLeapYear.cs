848977b58e96fc127317ff9751f593c6aa430394
VNKaiser <tandatvok16@gmail.com>
2023-10-05 08:23:55 +0700

    add check leap year

--- CheckLeapYear.cs
+++ CheckLeapYear.cs
@@ -1,10 +1,24 @@
-using System;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace UnitTestBDCL1
 {
-    class HamKT
+    class CheckLeapYear
     {
-        
+        public bool IsLeapYear(int n)
+        {
+            if ((n % 4 == 0) || (n % 100 == 0 && n % 400 == 0))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
 
     }
 }
---
+++ TestCheckLeapYear.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTestBDCL1
+{
+    [TestClass]
+    public class UnitTest3
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
